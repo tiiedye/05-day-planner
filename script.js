b9552fdf7675626dc249ff83493b8e2443e8603d
Julia Clements <tiiedye@gmail.com>
2020-05-01 07:46:42 -0600

    tinkering with getting textarea stuff to save

--- script.js
+++ script.js
@@ -1,75 +1,42 @@
 $(document).ready(function(){
 
-var timeSlot = [
-    {time: "09:00",
-    slot: "slot09",
-    save: "save09"},
-
-    {time: "10:00",
-    slot: "slot10",
-    save: "save10"},
-
-    {time: "11:00",
-    slot: "slot11",
-    save: "save11"},
-
-    {time: "12:00",
-    slot: "slot12",
-    save: "save12"},
-
-    {time: "13:00",
-    slot: "slot13",
-    save: "save13"},
-
-    {time: "14:00",
-    slot: "slot14",
-    save: "save14"},
-
-    {time: "15:00",
-    slot: "slot15",
-    save: "save15"},
-
-    {time: "16:00",
-    slot: "slot16",
-    save: "save16"},
-
-    {time: "17:00",
-    slot: "slot17",
-    save: "save17"}];
+var timeSlot = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : {}
 
 //Date and time
 $("#currentDay").append(Date($.now()));
+const date = new Date();
+const hour = date.getHours();
 
 //loads time, inputs, and save btns
-for (var i = 0; i < timeSlot.length; i++) {
+for (var i = 9; i < 18; i++) {
+    var row = $("<row>");
+    row.addClass("row");
+    $("#headtable").append(row);
+
     var timeDiv = $("<div>");
-    timeDiv.text(timeSlot[i].time);
-    timeDiv.addClass("col-md-2 hour");
+    timeDiv.text(`${i}:00`);
+    timeDiv.addClass("col-md-1 hour");
     $("#timeTable").append(timeDiv);
 
-    var inputDiv = $("<input>");
-    inputDiv.addClass("col-md-9 input time-block");
-    inputDiv.text(timeSlot[i].event);
-    inputDiv.attr("id", timeSlot[i].slot);
+    var inputDiv = $("<textarea>");
+    inputDiv.addClass(`col-md-10 input textarea ${i<hour? 'past' : i>hour? 'future': 'present'}`);
+    inputDiv.text(timeSlot[i]|| "");
+    inputDiv.attr("id", `todo${i}`);
     $("#timeTable").append(inputDiv);
 
     var saveBtnDiv = $("<button>");
     saveBtnDiv.addClass("col-md-1 saveBtn");
     saveBtnDiv.text("Save");
-    saveBtnDiv.attr("id", timeSlot[i].save);
+    saveBtnDiv.attr("id", i);
     $("#timeTable").append(saveBtnDiv);
 }
 
 //saves to local storage on button click
 $(".saveBtn").on("click", function(){
-    $(".input").each(function(){    
         var id = $(this).attr("id");
-        var value = $(this).val();
-       localStorage.setItem(id, value);
-    });   
-});
-
-//load from local storage
-
+        var value = $(this).siblings("textarea").val()
+        timeSlot[id] = value
+       localStorage.setItem('todos', JSON.stringify(timeSlot));
+});   
 
 });
